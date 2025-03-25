034a48897801403c5fd996eae1b5fdc14a1b918b
paulo4321-dra <paulo.franca@senacsp.edu.br>
2025-03-25 14:35:35 -0300

    Add files via upload

---
+++ arrayunidimensional-vetor/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arrayunidimensional_vetor
+{
+    public partial class frmVetor : Form
+    {
+        public frmVetor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTestar_Click(object sender, EventArgs e)
+        {
+            //Matriz de uma dimensão, também chamada de vetor
+            string[] pecasComputador = { "Mouse", "Teclado", "Monitor", "gabinete", "Câmera" };
+
+            //MessageBox.Show(pecascomputador[2]);
+
+            foreach (string peca in pecasComputador)
+                MessageBox.Show(peca);
+        }
+    }
+}
